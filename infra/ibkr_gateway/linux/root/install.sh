23350dbd5e3de677e28b0e841e1ee2100d53cf4a
Matt S <mlcs05@iapetus.localdomain>
2024-10-31 13:40:27 +0900

    working install script

--- infra/ibkr_gateway/linux/root/install.sh
+++ infra/ibkr_gateway/linux/root/install.sh
@@ -18,6 +18,7 @@ function log() {
 function setup-directories() {
     log "adding user"
 
+    groupadd -f ${APPGROUP}
     id -u ${APPUSER} &>/dev/null || useradd -m -s /bin/bash -g ${APPGROUP} ${APPUSER}
 
     log "Setting up ${APP_DIR}"
@@ -40,6 +41,7 @@ function setup-directories() {
         chown -R ${APPUSER}:${APPGROUP} ${CONFIG_DIR}
         chmod 755 ${CONFIG_DIR}
     }
+    log "finished setting up directories"
 }
 
 function install-configs() {
@@ -55,6 +57,35 @@ function install-configs() {
     cp ${repo_cfg_dir}/${APPUSER}/.env.tpl ${CONFIG_DIR}/.env.tpl
     sed -i "s/{{GATEWAY_PASSWORD}}/${PASSWORD}/g" ${CONFIG_DIR}/.env.tpl
     mv ${CONFIG_DIR}/.env.tpl ${CONFIG_DIR}/.env
+
+    chown ${APPUSER}:${APPGROUP} ${CONFIG_DIR}/.env
+    chmod 755 ${CONFIG_DIR}/.env
+
+    log "finished installing configuration"
+}
+
+function install-app() {
+    log "installing ${APPUSER} app"
+    local repo_app_dir=${REPOSITORY}/python
+    local current_dir=$(pwd)
+    log "copying py files"
+    cp ${repo_app_dir}/*.py ${APP_DIR}/
+    log "copying requirements"
+    cp ${repo_app_dir}/requirements.txt ${APP_DIR}/
+    cd ${APP_DIR}
+    log "creating venv"
+    python3 -m venv venv
+    log "activating venv"
+    source ./venv/bin/activate
+    log "installing requirements"
+    pip3 install -r requirements.txt
+    log "deactivating venv"
+    deactivate
+    log "setting permissions"
+    chown -R ${APPUSER}:${APPGROUP} ${APP_DIR}
+    chmod 755 ${APP_DIR}/*
+    cd ${current_dir}
+    log "finished installing ${APPUSER} app"
 }
 
 function install-service() {
@@ -71,10 +102,12 @@ function install-service() {
     log "enabling and starting ${APPUSER}"
     systemctl enable ${APPUSER}
     systemctl restart ${APPUSER}
+
+    log "finished installing ${APPUSER} service"
 }
 
 function remove-old-log() {
-    [ -f "${INSTALL_LOG}" ] && rm "${INSTALL_LOG}"
+    rm -f "${INSTALL_LOG}"
 }
 
 function check-repository-in-root() {
@@ -90,14 +123,50 @@ function check-root() {
     }
 }
 
-function main() {
+function install() {
     check-root
     check-repository-in-root
     remove-old-log
+    log "starting install"
     setup-directories
     install-configs
+    install-app
     install-service
     log "install complete"
 }
 
-main
+function uninstall() {
+    check-root
+    check-repository-in-root
+    log "starting uninstall"
+    log "stopping ${APPUSER}"
+    systemctl stop ${APPUSER}
+    log "disabling ${APPUSER}"
+    systemctl disable ${APPUSER}
+    log "removing ${APPUSER} service"
+    rm /etc/systemd/system/${APPUSER}.service
+    log "reloading daemon"
+    systemctl daemon-reload
+    log "removing ${APPUSER} app"
+    rm -rf ${APP_DIR}
+    log "removing ${APPUSER} config"
+    rm -rf ${CONFIG_DIR}
+    log "removing ${APPUSER} logs"
+    rm -rf ${LOG_DIR}
+    log "removing ${APPUSER} user"
+    userdel -r ${APPUSER}
+    log "uninstall complete"
+}
+
+function main() {
+    if [ "$1" = "-u" ]; then
+            log "uninstalling"
+            uninstall
+        else
+            log "installing"
+            install
+        fi
+    
+}
+
+main "$@"
